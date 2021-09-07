6d8c08c0e5f62721c2eea68131bac9c6f3420e70
elysium09 <eyueldaniel0921@gmail.com>
2021-09-07 14:00:16 +0300

    :sparkles: DB class refactor

--- app/src/main/java/com/codexo/notes/data/NoteDatabase.kt
+++ app/src/main/java/com/codexo/notes/data/NoteDatabase.kt
@@ -1,34 +1,9 @@
 package com.codexo.notes.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Note::class], version = 1)
 abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: NoteDatabase? = null
-
-        fun getInstance(context: Context): NoteDatabase {
-
-            synchronized(this) {
-                var instance = INSTANCE
-
-                if (instance == null) {
-                    instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        NoteDatabase::class.java,
-                        "notes_db"
-                    ).fallbackToDestructiveMigration().build()
-                    INSTANCE = instance
-                }
-                return instance
-            }
-        }
-    }
-
 }
--- app/src/main/java/com/codexo/notes/di/DatabaseModule.kt
+++ app/src/main/java/com/codexo/notes/di/DatabaseModule.kt
@@ -1,4 +1,4 @@
-package com.codexo.notes.core
+package com.codexo.notes.di
 
 import android.content.Context
 import androidx.room.Room
